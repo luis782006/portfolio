f719a84a4a2565f79fd6f18587a3db1eddcfbe7f
luis782006 <luisfelipe782006@gmail.com>
2022-07-20 11:07:37 -0300

    libreria ngbBootstrap mas modal editar acerca de mi

--- AngularPortfolio/src/app/app.module.ts
+++ AngularPortfolio/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +33,8 @@ import { EditAcercaComponent } from './componentes/edit-acerca/edit-acerca.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularPortfolio/src/app/componentes/acerca/acerca.component.ts
+++ AngularPortfolio/src/app/componentes/acerca/acerca.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {faPen} from '@fortawesome/free-solid-svg-icons';
+import { NgbActiveModal, NgbAlert, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-acerca',
@@ -10,27 +11,24 @@ export class AcercaComponent implements OnInit {
 
   faPen = faPen;
 
-  editAcercaVisible:boolean;
-  acercaVisible:boolean=true;
-  acercaEditVisible:boolean;
   pathFoto:string="../../../assets/profile-photo.jpg";
   miNombre:string="Luis F Sanchez Barranco";
   descripcion:string="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium, nihil debitis? Mollitia tenetur aspernatur illo corrupti cumque, quos saepe amet molestiae aliquam eum iusto voluptatum iure nihil perspiciatis fugiat dolor!"
   
  
 
-  constructor() { }
+  constructor(private modalPersona:NgbModal) { }
 
   ngOnInit(): void {
     
   }
-  openEditAcerca(){
-    this.editAcercaVisible=true;
-    this.acercaVisible=false;
+  openEditAcerca(modalPersona){
+   
+    this.modalPersona.open(modalPersona);
   }
   cambioEditVisible(e){ // e de evento. obejto que se recibe.desde el output.component hijo.
-    this.editAcercaVisible=e;
-    this.acercaVisible=true;
+    //this.editAcercaVisible=e;
+    //this.acercaVisible=true;
   }
 
 
